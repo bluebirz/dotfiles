4596cfaba2ba33f47d266f0959f2bf7f8ec1b6a0
bluebirz <9207891+bluebirz@users.noreply.github.com>
2024-06-23 00:43:14 +0200

    custom tmux tab + lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
+      opts.options = { theme = "nightfly" }
       opts.sections.lualine_c = { { "filename", path = 1 } }
       opts.sections.lualine_z = {}
     end,
