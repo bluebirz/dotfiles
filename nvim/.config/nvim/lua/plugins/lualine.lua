cfbcac66acd3a1818aa38d83cba881ff22c50cf2
bluebirz <9207891+bluebirz@users.noreply.github.com>
2024-06-23 10:10:29 +0200

    config lualine + tmux top

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,17 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", "mikesmithgh/git-prompt-string-lualine.nvim" },
     opts = function(_, opts)
-      opts.options = { theme = "nightfly" }
-      opts.sections.lualine_c = { { "filename", path = 1 } }
+      opts.options = {
+        theme = "codedark",
+        -- component_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = ''},
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      }
+      opts.sections.lualine_b = { "git_prompt_string", "diff", "diagnostics" }
+      opts.sections.lualine_c = { { "filename", path = 4 } }
       opts.sections.lualine_z = {}
     end,
   },
