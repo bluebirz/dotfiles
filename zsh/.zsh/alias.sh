07620969a81820120256b7e517b8151894a8f405
bluebirz <9207891+bluebirz@users.noreply.github.com>
2024-11-12 13:51:53 +0100

    update ohmyposh icon + alias for upd

--- zsh/.zsh/alias.sh
+++ zsh/.zsh/alias.sh
@@ -1,12 +1,16 @@
 alias lsofp="sudo lsof -iTCP -sTCP:LISTEN -n -P"
 upd() (
   HIGHLIGHT="\033[1;44m"
+  TIMESTAMPER="\033[31m"
   RESET="\033[0m"
   PREFIX="\$ Updating "
   SUFFIX="... "
+
+  echo ${TIMESTAMPER}'Running at '$(date -u "+%Y-%m-%d %H:%M:%S")${RESET}
   echo ${HIGHLIGHT}${PREFIX}'Homebrew'${SUFFIX}${RESET} && brew update && brew upgrade
   echo ${HIGHLIGHT}${PREFIX}'GCloud'${SUFFIX}${RESET} && echo 'Y' | gcloud components update
   echo ${HIGHLIGHT}${PREFIX}'Devbox'${SUFFIX}${RESET} && devbox version update
+  echo ${TIMESTAMPER}'Finished at '$(date -u "+%Y-%m-%d %H:%M:%S")${RESET}
 )
 
 alias nv="nvim ." # nvim
@@ -14,3 +18,5 @@ alias zl="zellij"
 # alias prep_py="python3 -m venv venv && echo \"source venv/bin/activate\nlayout python\" > .envrc && direnv allow"
 alias dus="du -h -d 1 | sort -h"
 alias lsh="ls -lah | sort -h"
+alias dvb="devbox"
+alias tmx="tmux attach"
