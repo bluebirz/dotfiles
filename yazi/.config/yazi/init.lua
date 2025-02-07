c628b2669bf33a7040782d5e4b1fb66a487f2855
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-02-08 00:37:58 +0100

    decor log in lazygit + update alias + yazi git status

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,17 +1,17 @@
 -- git
--- THEME.git = THEME.git or {}
--- THEME.git.modified = ui.Style():fg("blue")
--- THEME.git.deleted = ui.Style():fg("red"):bold()
+THEME.git = THEME.git or {}
+THEME.git.modified = ui.Style():fg("yellow"):bold()
+THEME.git.deleted = ui.Style():fg("red"):bold()
 -- THEME.git.untracked
 -- THEME.git.ignored
--- THEME.git.deleted
+-- THEME.git.added
 -- THEME.git.updated
--- THEME.git.modified_sign = "M"
--- THEME.git.deleted_sign = "D"
--- THEME.git.added_sign
--- THEME.git.untracked_sign
--- THEME.git.ignored_sign
--- THEME.git.updated_sign
+THEME.git.modified_sign = "M"
+THEME.git.deleted_sign = "D"
+THEME.git.added_sign = "A"
+THEME.git.untracked_sign = "?"
+THEME.git.ignored_sign = "I"
+THEME.git.updated_sign = "U"
 require("git"):setup()
 
 -- full-border
