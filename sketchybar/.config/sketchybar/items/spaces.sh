7c754dde7890a38c3791c57cefd0d592eceef466
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-02-11 16:59:51 +0100

    tweak color of space

--- sketchybar/.config/sketchybar/items/spaces.sh
+++ sketchybar/.config/sketchybar/items/spaces.sh
@@ -3,6 +3,9 @@ source $HOME/.config/sketchybar/color.sh
 source $HOME/.config/sketchybar/constants.sh
 
 SPACE_ICONS=("一" "二" "三" "四" "五" "六" "七" "八" "九" "十")
+# SPACE_ICONS=("󰬺" "󰬻" "󰬼" "󰬽" "󰬾" "󰬿" "󰭀" "󰭁" "󰭂" "󰿩")
+# SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+# SPACE_ICONS=("๑" "๒" "๓" "๔" "๕" "๖" "๗" "๘" "๙" "๑๐")
 
 whole_spaces_opts=(
   background.color=$SPACESET_BACKGROUND_COLOR
