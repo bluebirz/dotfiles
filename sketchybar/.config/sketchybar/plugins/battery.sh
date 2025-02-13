0a8898de4f1c47511aace83b8e8c455dc7261f22
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-02-13 15:29:12 +0100

    custom colors + battery icons

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -7,22 +7,34 @@ if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-case "${PERCENTAGE}" in
-100) ICON="" ;;
-9[0-9]) ICON="󰂂" ;;
-8[0-9]) ICON="󰂁" ;;
-7[0-9]) ICON="󰂀" ;;
-6[0-9]) ICON="󰁿" ;;
-5[0-9]) ICON="󰁾" ;;
-4[0-9]) ICON="󰁽" ;;
-3[0-9]) ICON="󰁼" ;;
-2[0-9]) ICON="󰁻" ;;
-1[0-9]) ICON="󰁺" ;;
-*) ICON="󰂎" ;;
-esac
-
 if [[ "$CHARGING" != "" ]]; then
-  ICON="󰂄"
+  case "${PERCENTAGE}" in
+  100) ICON="󰂅" ;;
+  9[0-9]) ICON="󰂋" ;;
+  8[0-9]) ICON="󰂊" ;;
+  7[0-9]) ICON="󰢞" ;;
+  6[0-9]) ICON="󰂉" ;;
+  5[0-9]) ICON="󰢝" ;;
+  4[0-9]) ICON="󰂈" ;;
+  3[0-9]) ICON="󰂇" ;;
+  2[0-9]) ICON="󰂆" ;;
+  1[0-9]) ICON="󰢜" ;;
+  *) ICON="󰢟" ;;
+  esac
+else
+  case "${PERCENTAGE}" in
+  100) ICON="󰁹" ;;
+  9[0-9]) ICON="󰂂" ;;
+  8[0-9]) ICON="󰂁" ;;
+  7[0-9]) ICON="󰂀" ;;
+  6[0-9]) ICON="󰁿" ;;
+  5[0-9]) ICON="󰁾" ;;
+  4[0-9]) ICON="󰁽" ;;
+  3[0-9]) ICON="󰁼" ;;
+  2[0-9]) ICON="󰁻" ;;
+  1[0-9]) ICON="󰁺" ;;
+  *) ICON="󰂎" ;;
+  esac
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
