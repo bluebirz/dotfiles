30c3e0e5bb4f64bc5134645a5b726954d3654e03
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-03-01 23:56:28 +0100

    cleanup unused nvim plugin + zsh not check pkg to reduce response time

--- zsh/.zsh/addons.sh
+++ zsh/.zsh/addons.sh
@@ -1,29 +1,29 @@
 # enable zoxide
-if brew ls --versions zoxide >/dev/null; then
-  eval "$(zoxide init --cmd cd zsh)"
-fi
+# if brew ls --versions zoxide >/dev/null; then
+eval "$(zoxide init --cmd cd zsh)"
+# fi
 
 # Enable atuin
-if brew ls --versions atuin >/dev/null; then
-  eval "$(atuin init zsh)"
-fi
+# if brew ls --versions atuin >/dev/null; then
+eval "$(atuin init zsh)"
+# fi
 
 # direnv
-if brew ls --versions direnv >/dev/null; then
-  eval "$(direnv hook zsh)"
-fi
+# if brew ls --versions direnv >/dev/null; then
+eval "$(direnv hook zsh)"
+# fi
 
 # devbox
-if brew ls --versions devbox >/dev/null; then
-  eval "$(devbox global shellenv --init-hook --recompute)"
-fi
+# if brew ls --versions devbox >/dev/null; then
+eval "$(devbox global shellenv --init-hook --recompute)"
+# fi
 
 # zsh-autosuggestions
-if brew ls --versions zsh-autosuggestions >/dev/null; then
-  source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-fi
+# if brew ls --versions zsh-autosuggestions >/dev/null; then
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+# fi
 
 # zsh-syntax-highlighting
-if brew ls --versions zsh-syntax-highlighting >/dev/null; then
-  source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
+# if brew ls --versions zsh-syntax-highlighting >/dev/null; then
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# fi
