320b2d6acf1317974596fc44c005b8ccf0505d7a
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-03-02 01:04:41 +0100

    custom header

--- nvim/.config/nvim/lua/plugins/alpha-nvim.lua
+++ nvim/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -2,6 +2,29 @@ return {
   "goolord/alpha-nvim",
   dependencies = { "echasnovski/mini.icons" },
   config = function()
-    require("alpha").setup(require("alpha.themes.startify").config)
+    -- require("alpha").setup(require("alpha.themes.startify").config)
+    local alpha = require("alpha")
+    local startify = require("alpha.themes.startify")
+    startify.section.header.val = {
+      -- [[                                   __                ]],
+      -- [[      ___     ___    ___   __  __ /\_\    ___ ___    ]],
+      -- [[     / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+      -- [[    /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+      -- [[    \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+      -- [[     \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+      [[                              ]],
+      [[   ,--.   ,--.                ]],
+      [[   |  |-. |  |,--.,--. ,---.  ]],
+      [[   | .-. '|  ||  ||  || .-. : ]],
+      [[   | `-' ||  |'  ''  '\   --. ]],
+      [[    `---' `--' `----'  `----' ]],
+      [[   ,--.   ,--.                ]],
+      [[   |  |-. `--',--.--.,-----.  ]],
+      [[   | .-. ',--.|  .--'`-.  /   ]],
+      [[   | `-' ||  ||  |    /  `-.  ]],
+      [[    `---' `--'`--'   `-----'  ]],
+      [[                              ]],
+    }
+    alpha.setup(startify.config)
   end,
 }
